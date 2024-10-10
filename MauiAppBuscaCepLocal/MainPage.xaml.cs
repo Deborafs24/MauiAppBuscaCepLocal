df3b1d3356f8082f754abb8dfd8c7f15f555d1d8
Usuario <Usuario@CPD03-M02>
2024-10-10 15:15:25 -0300

    Continuação com API

--- MauiAppBuscaCepLocal/MainPage.xaml.cs
+++ MauiAppBuscaCepLocal/MainPage.xaml.cs
@@ -1,9 +1,10 @@
-﻿namespace MauiAppBuscaCepLocal
+﻿using MauiAppBuscaCepLocal.Model;
+using MauiAppBuscaCepLocal.Services;
+
+namespace MauiAppBuscaCepLocal
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
@@ -11,18 +12,22 @@
 
         private void OnCounterClicked(object sender, EventArgs e)
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+           
         }
 
-        private void Button_Clicked(object sender, EventArgs e)
+        private async void Button_Clicked(object sender, EventArgs e)
         {
+            try
+            {
+                Endereco? dados_endereco =
+                    await DataServiceCep.GetEnderecoByCep(txt_cep.Text);
+
+                BindingContext = dados_endereco;
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops", ex.Message, "OK");
+            }
 
         }
     }
